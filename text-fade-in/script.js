337bac799f150f047326a9339b430b74a65f2072
Katharina Spiecker <katharina.spiecker@solution360.de>
2022-09-17 15:47:20 +0200

    refactor

--- text-fade-in/script.js
+++ text-fade-in/script.js
@@ -1,22 +1,18 @@
-let headline = document.querySelector('#headline');
-let splitHeadline = headline.innerText.split('');
-let newHeadline = '';
-splitHeadline.forEach(char => {
-	newHeadline += '<span>' + char + '</span>';
+const headline = document.querySelector("#headline");
+const splitHeadline = headline.innerText.split("");
+let newHeadline = "";
+splitHeadline.forEach((char) => {
+    newHeadline += "<span>" + char + "</span>";
 });
 
 headline.innerHTML = newHeadline;
-index = 0;
-const spans = headline.querySelectorAll('span');
-let timer = setInterval(onTick, 50);
-function onTick(){
-	spans[index].classList.add('fade');
-	index++;
-	if(index === spans.length){
-		complete();
-	}
-}
-
-function complete(){
-	clearInterval(timer);
+let index = 0;
+const spans = headline.querySelectorAll("span");
+const timer = setInterval(onTick, 50);
+function onTick() {
+    spans[index].classList.add("fade");
+    index++;
+    if (index === spans.length) {
+        clearInterval(timer);
+    }
 }
--- text-typing/script.js
+++ text-typing/script.js
@@ -1,19 +1,19 @@
 const targetText = document.querySelector(".typewriter");
 const splitTargetText = targetText.innerText.split("");
 let newTargetText = "";
-splitTargetText.forEach(character => {
+splitTargetText.forEach((character) => {
     newTargetText += "<span>" + character + "</span>";
-})
+});
 
 targetText.innerHTML = newTargetText;
 let index = 0;
 const spans = targetText.querySelectorAll("span");
 const timer = setInterval(onTick, 250);
 
-function onTick(){
+function onTick() {
     spans[index].style = "display:inline-block";
     index++;
-    if(index === spans.length){
+    if (index === spans.length) {
         clearInterval(timer);
     }
 }
